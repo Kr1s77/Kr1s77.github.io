54eea9c6cb857757faf53f89ddd538860f7ad784
kris <criselyj@163.com>
2021-07-14 10:27:12 +0800

    new

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 # _*_ coding: utf-8 _*_
-import sys
+import flask
+
+app = flask.Flask(__name__)
 
 
-print sys.argv[1]
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=443)
